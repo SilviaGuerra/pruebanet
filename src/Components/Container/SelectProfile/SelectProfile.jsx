e310466e67e957295971788ce9778009391cd064
SilviaGuerra <silvia.guerrahdz@gmail.com>
2018-09-24 19:17:04 -0500

    fotos de perfil

--- src/Components/Container/SelectProfile/SelectProfile.jsx
+++ src/Components/Container/SelectProfile/SelectProfile.jsx
@@ -4,7 +4,23 @@ import selectProfileStyles from './SelectProfileStyles';
 const SelectProfile = () => {
     return (
         <div {...selectProfileStyles()}>
-            jajaja alo
+            <h1>Quien esta viendo ahora?</h1>
+            <div className="user__photo">
+                <img src="https://i.pinimg.com/originals/08/90/78/089078150f5f43e817c4fd2491ef6e30.jpg" alt=""/>
+                <h3>Nombre</h3>
+            </div>
+            <div className="user__photo">
+                <img src="https://i.pinimg.com/originals/08/90/78/089078150f5f43e817c4fd2491ef6e30.jpg" alt=""/>
+                <h3>Nombre</h3>
+            </div>
+            <div className="user__photo">
+                <img src="https://i.pinimg.com/originals/08/90/78/089078150f5f43e817c4fd2491ef6e30.jpg" alt=""/>
+                <h3>Nombre</h3>
+            </div>
+            <div className="user__photo">
+                <img src="https://i.pinimg.com/originals/08/90/78/089078150f5f43e817c4fd2491ef6e30.jpg" alt=""/>
+                <h3>Nombre</h3>
+            </div>
         </div>
     );
 }
--- src/Components/Container/SelectProfile/SelectProfileStyles.js
+++ src/Components/Container/SelectProfile/SelectProfileStyles.js
@@ -1,7 +1,14 @@
 import { css } from 'glamor';
 
 const selectProfileStyles = () => css({
-    color: '#fff'
+    color: '#fff',
+    ' .user__photo': {
+        display: 'inline-block',
+        ' img': {
+            height: 120,
+            borderRadius: '500px'
+        }
+    }
 });
 
 export default selectProfileStyles;
